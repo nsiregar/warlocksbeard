e6cfe8fab602ea32754491190f43a2476db01a59
Ngalim Siregar <ngalim.siregar@gmail.com>
2018-06-19 06:30:11 +0900

    remove all compiled python files

--- tasks.py
+++ tasks.py
@@ -9,3 +9,8 @@ def server(ctx):
         ctx.run('gunicorn --bind 0.0.0.0:8000  src:app', pty=True)
     if platform == 'win32':
         ctx.run('waitress-serve --host=0.0.0.0 --port=8000 src:app', pty=True)
+
+@task
+def cleanup(ctx):
+    print('Clean up *.pyc files')
+    ctx.run()
